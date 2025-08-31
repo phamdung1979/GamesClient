f5c4354405a1a2fbebf3b71639b47bdbd8fff6c8
khacquank2 <khacquanngu@gmail.com>
2025-08-31 10:45:51 +0700

    Cap nhat du lieu game noi bat

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-const games = document.querySelector("#games");
+const games = document.querySelector('#popular-game')
+
+const getData = async () => {
+    const response = await fetch("./data/gamepopular.json");
+    let data = await response.json();
+    if(data){
+        games.innerHTML = data.map(item => {
+            return `
+            <div class="card" id="game">
+                <img src="${item.image}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">${item.title}</h5>
+                    <p class="card-text">${item.description}</p>
+                    <a href="./game_detali3.html" class="btn btn-dark">Xem chi tiết</a>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">${item.update}</small>
+                </div>
+            </div>
+            `
+        }).join('')
+    }
+};
+
+getData();
